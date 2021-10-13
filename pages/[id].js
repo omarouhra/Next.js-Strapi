6e898b0e561e92ad06c9f42c5fb044923d7dfdc1
Omar Ouhra <omarmaroc26@gmail.com>
2021-10-13 18:17:03 +0100

    Categories

--- pages/[id].js
+++ pages/[id].js
@@ -1,16 +1,24 @@
 import axios from "axios";
-function post({ post }) {
+function post({ post, posts }) {
+  const otherPosts = posts.filter(article => article.id != post.id);
+  console.log("thes are other posts", otherPosts);
   return (
     <div>
       <h1>{post.title} </h1>
       <h2>{post.description}</h2>
       <p>{post.content}</p>
+      {post.image ?(<img src={post.image.name} alt='' />):<p>no image founded</p>}
+      <h2> more Projects</h2>
+      {otherPosts.map(post => (
+        <div key={post.id}>
+          <a href={`/${post.id}`}>{post.title}</a>
+        </div>
+      ))}
     </div>
   );
 }
 
 export default post;
-
 
 // * static Side generated dynamic routing
 // export async function getStaticProps({ params }) {
@@ -40,10 +48,12 @@ export async function getServerSideProps(context) {
   const { params } = context;
   const { id } = params;
   const post = await axios.get(`http://localhost:1337/posts/${id}`);
+  const posts = await axios.get("http://localhost:1337/posts");
 
   return {
     props: {
       post: post.data,
+      posts: posts.data,
     },
   };
 }
--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,15 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import axios from "axios";
 import Link from "next/link";
+import { useState } from "react";
 
-export default function Home({ posts }) {
-  console.log(posts);
+export default function Home({ posts, categories }) {
+  const [filter, setFilter] = useState(1);
+
+  let projects = posts.filter(post => post.category.id === filter);
+  const setprojects = id => {
+    setFilter(id);
+  };
   return (
     <div className={styles.container}>
       <Head>
@@ -15,11 +21,20 @@ export default function Home({ posts }) {
       </Head>
       <h1>Hello World</h1>
       <h2>Strapi Demo</h2>
-      {posts.map(post => (
+      {categories.map(cat => (
+        <button
+          key={cat.id}
+          onClick={() => {
+            setFilter(cat.id);
+          }}>
+          {cat.name}
+        </button>
+      ))}
+      {projects.map(post => (
         <div key={post.id}>
           <h1>{post.title}</h1>
           <h2>{post.description}</h2>
-          <Link href={`/${post.id}`}>read the blog</Link>
+          <Link href={`/${post.id}`}>see more</Link>
         </div>
       ))}
     </div>
@@ -27,10 +42,12 @@ export default function Home({ posts }) {
 }
 export async function getServerSideProps() {
   const posts = await axios.get("http://localhost:1337/posts");
+  const categories = await axios.get("http://localhost:1337/categories");
 
   return {
     props: {
       posts: posts.data,
+      categories: categories.data,
     },
   };
 }
